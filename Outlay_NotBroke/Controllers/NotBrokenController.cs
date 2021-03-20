da08aca1f46a2b1ff6f0ec65f244b38676d16a8e
Serhii Yanchuk <novograd2201@gmail.com>
2021-03-20 11:17:40 +0200

    lab2_istio_init

---
+++ Outlay_NotBroke/Controllers/NotBrokenController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Outlay_NotBroke.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class NotBrokenController : ControllerBase
+    {
+        
+        private readonly ILogger<NotBrokenController> _logger;
+
+        public NotBrokenController(ILogger<NotBrokenController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("break")]
+        public string GetBreak([FromServices] IGood goodService)
+        {
+            goodService.isGood = false;
+            return "Service is broken";
+        }
+
+        [HttpGet]
+        [Route("service")]
+        public string GetService([FromServices] IGood goodService)
+        {
+            if (goodService.isGood)
+                return "Service works really fast!!! Hi from normal service";
+            else 
+            {
+                Thread.Sleep(20000);
+                return "Hi from broke service, I'm very slow";
+            }
+        }
+    }
+}
---
+++ Outlay_Users/Controllers/TestServiceController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Outlay_Users.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class TestServiceController : ControllerBase
+    {
+        private readonly ILogger<TestServiceController> _logger;
+
+        public TestServiceController(ILogger<TestServiceController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("test")]
+        public string GetService()
+        {
+            string responseString = "";
+            string url = "http://localhost/api/service";
+            double totalTime = 0;
+            DateTime CurDate = DateTime.Now;
+
+            WebRequest request;
+
+            try
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    request = WebRequest.Create(url);
+                    WebResponse response = request.GetResponse();
+                    using (Stream stream = response.GetResponseStream())
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            responseString += reader.ReadToEnd();
+                        }
+                    }
+                    response.Close();
+                    
+                }
+            }
+            catch (System.Exception)
+            {
+                responseString = "Error occured!";
+            }
+
+            TimeSpan span = DateTime.Now - CurDate;
+            totalTime = span.TotalSeconds;
+            responseString += " \nTotal time: " + totalTime + " seconds";
+
+            return responseString;
+        }
+    }
+}
