7c55bd0d440377119c1c188eeff7883a6fa80ce3
Serhii Yanchuk <novograd2201@gmail.com>
2021-04-09 11:50:35 +0300

    init kafka

---
+++ ProducerMicroservice/ProducerMicroservice/Controllers/KafkaProducerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ProducerMicroservice.Controllers
+{
+    [Route("api/kafka")]
+    [ApiController]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ProducerConfig config = new ProducerConfig
+                                { BootstrapServers = "kafka:9092"};
+        private readonly string topic = "simpletalk_topic";
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Created(string.Empty, SendToKafka(topic, "omg"));
+        }
+        private Object SendToKafka(string topic, string message)
+        {
+            using (var producer =
+                 new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    return producer.ProduceAsync(topic, new Message<Null, string> { Value = message })
+                        .GetAwaiter()
+                        .GetResult();
+                    //producer.Produce(topic, new Message<Null, string> { Value = message });
+                }
+                catch (Exception e)
+                {
+                    return $"Oops, something went wrong: {e}";
+                }
+            }
+            return "null";
+        }
+    }
+}
---
+++ TransactionMicroservice/TransactionMicroservice/Controllers/TransactionController.cs
@@ -0,0 +1,75 @@
+﻿using CardMicroservice.Models;
+using CardMicroservice.UoW;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TransactionController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //[HttpGet("{id}", Name = "Get")]
+        //public IActionResult Get(int id)
+        //{
+        //    try
+        //    {
+        //        var transaction = _unitOfWork.Transactions.Get(id);
+        //        if (transaction == null)
+        //            return new NotFoundResult();
+
+        //        return new OkObjectResult(transaction);
+        //    }
+        //    catch
+        //    {
+        //        return new BadRequestResult();
+        //    }
+        //}
+
+        [Route("/cardAllTransaction")]
+        [HttpGet]
+        public ActionResult<CardTransaction> Get([FromBody]int cardId)
+        {
+            try
+            {
+                var transactions = _unitOfWork.Transactions.Find(x => x.Card.Id == cardId);
+                if (transactions == null)
+                    return new NotFoundResult();
+
+                return new OkObjectResult(transactions);
+            }
+            catch
+            {
+                return new BadRequestResult();
+            }
+        }
+
+        [Route("/cardTransactionByType")]
+        [HttpGet]
+        public ActionResult<CardTransaction> Get([FromBody]int cardId, [FromBody] int type)
+        {
+            try
+            {
+                var transactions = _unitOfWork.Transactions.Find(x => x.Card.Id == cardId && x.Type == (TransactionType)type);
+                if (transactions == null)
+                    return new NotFoundResult();
+
+                return new OkObjectResult(transactions);
+            }
+            catch
+            {
+                return new BadRequestResult();
+            }
+        }
+    }
+}
